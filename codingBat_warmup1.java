7a6bef6873f5fb0913c67ebf7285f5f62084b6d2
David J. Davis <djdavis@mail.wvu.edu>
2016-06-10 06:54:34 -0400

    Another Problem solved.

--- codingBat_warmup1.java
+++ codingBat_warmup1.java
@@ -31,3 +31,20 @@ public boolean parrotTrouble(boolean talking, int hour) {
       return false; 
   }
 }
+
+// Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
+// 
+// makes10(9, 10) → true
+// makes10(9, 9) → false
+// makes10(1, 9) → true
+
+public boolean makes10(int a, int b){
+    int num = 10; 
+    int c = a + b; 
+    if(a == 10 || b == 10 || c == 10){ 
+        return true;     
+    } else { 
+        return false;
+    }
+}
+
